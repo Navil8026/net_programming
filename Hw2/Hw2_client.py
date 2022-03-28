217984f832d88a123a5394357801eb4b1b8178db
Navil8026 <cym8026@naver.com>
2022-03-28 16:40:12 +0900

    Hw2

---
+++ Hw2/Hw2_client.py
@@ -0,0 +1,11 @@
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+addr = ('localhost',9000)
+sock.connect(addr)
+
+msg = sock.recv(1024)
+print(msg.decode())
+sock.send(b'Youngmin Cha')
+print(int.from_bytes(sock.recv(1024),'big'))
+sock.close()
